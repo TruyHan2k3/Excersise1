63c38d30ea2869580ec98b254ce317caf169a494
TranNgocHuy <TranNgocHuy@DESKTOP-4PIU94M>
2024-10-22 19:55:54 +0700

    Add project files.

---
+++ Excersise1/Controllers/ChuDeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Excersise1.Models;
+
+namespace Excersise1.Controllers
+{
+    public class ChuDeController : Controller
+    {
+        // GET: ChuDe
+
+        QLBANSACHEntities db = new QLBANSACHEntities();
+        public ActionResult Index()
+        {
+            var All_ChuDe = from tt in db.CHUDEs select tt;
+            return View(All_ChuDe);
+        }
+    }
+}
